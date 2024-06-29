92359154c2527f26cb61b1c0ec3a3e7495b38346
adaowls439 <adaow1257@gmail.com>
2024-06-29 08:45:28 -0300

    v1.3.1 Ajuste no texto

--- conversor.py
+++ conversor.py
@@ -102,7 +102,7 @@ def process_videos_in_folder(input_folder, output_folder, config_file):
         with open(('Instrucoes.txt'), 'w') as f:
             f.write("Instruções de Uso:\n")
             f.write("- Organize seus vídeos na pasta 'Input';\n")
-            f.write("- O script converterá e comprimirá os vídeos na pasta 'Output';\n")
+            f.write("- O script converterá e comprimirá os vídeos para a pasta 'Output';\n")
             f.write("- Altere as configurações em Config.txt;\n")
             f.write("- O arquivo pode ser convertido varias vezes até chegar ao tamanho desejado;\n")
             f.write("- Certifique-se de que os arquivos de vídeo sejam no formato .mp4.\n")
